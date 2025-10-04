2132491b5d01cc9d23d84f0b79d6f8b35b6cd3ea
unknown <adewalefawas01@gmail.com>
2025-10-04 21:05:44 +0100

    java version

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y
 
 # Install Java SDK 11
-sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 
 # Download and Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
